b1a723429ebe4aafe9b7bab5cf12057984ef7741
Subham <chromaticshubham26@gmail.com>
2024-02-18 23:43:23 +0530

    to complete login details

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login'
 
 const App = () => {
   return (
-    <div>
-      hi there 
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
